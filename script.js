040d27a47706b310ea1182dab794fd971e0fece4
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-07-17 15:01:48 -0700

    Basic Calculator Layout

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector("container");
+const screen = document.querySelector("screen");
+const number = document.querySelector("number");
+const operate = document.querySelector("operate");
+const buttons = document.querySelectorAll("button");
+

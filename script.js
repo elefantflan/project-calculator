85b81eecc3c50066ea48349f4247d4a466502e5d
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-07-17 15:16:33 -0700

    Screen Outputs Button 1 and +

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const container = document.querySelector("container");
-const screen = document.querySelector("screen");
-const number = document.querySelector("number");
-const operate = document.querySelector("operate");
-const buttons = document.querySelectorAll("button");
+const container = document.querySelector(".container");
+const screen = document.querySelector(".screen");
+const number = document.querySelector(".number");
+const operate = document.querySelector(".operate");
+const buttons = document.querySelectorAll(".button");
 
+//screen respond to button press
+const one = document.querySelector("#one");
+const add = document.querySelector("#add");
+const equal = document.querySelector("#equal");
+
+one.addEventListener("click", ()=>
+{
+    screen.textContent += Number(one.textContent);
+}
+)
+
+add.addEventListener("click", ()=>
+{
+    screen.textContent += "+";
+}
+);
